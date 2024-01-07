bfe40a976b37a7855840f8fc6b24fd70061c2dea
Khateeb Ahmad <khateebahmad69@gmail.com>
2024-01-08 01:27:45 +0530

    change css

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 
-import "./LoginForm.css";
-
 const LoginForm = () => {;
   const [dropdown1, setDrowpdown1] = useState("");
   const [dropdown2, setDrowpdown2] = useState("");
@@ -22,13 +20,13 @@ const LoginForm = () => {;
   };
 
   return (
-    <div className="wrapper bg-dark d-flex align-items-center justify-content-center w-100">
-      <div className="loginform rounded">
+    <div className="wrapper bg-dark d-flex align-items-center justify-content-center vh-100 ">
+      <div className="loginform rounded  p-3  bg-light bg-gradient" style={{width: "600px"}}>
         <h2 className="mb-3">Login Form</h2>
         <form onSubmit={handleSubmit}>
           <div className="form-group mb-2 row">
             <label className="form-label col-sm-3 col-md-3 col-lg-3 label1" >Drowpdown 1</label>
-            <select className=" form-control col auto" value={dropdown1} onChange={handleChange1}>
+            <select className=" form-control col auto " style={{cursor:"pointer"}} value={dropdown1} onChange={handleChange1}>
               <option value={0}>none</option>
               <option value={1}>1st item</option>
               <option value={2}>2nd item</option>
@@ -38,7 +36,7 @@ const LoginForm = () => {;
           </div>
           <div className="form-group mb-2 row">
             <label className="form-label col-sm-3 col-md-3 col-lg-3">Drowpdown 2</label>
-            <select className="form-control col auto" value={dropdown2} onChange={handleChange2}>
+            <select className="form-control col auto  " style={{cursor:"pointer"}} value={dropdown2} onChange={handleChange2}>
               <option value={0}>none</option>
               <option value={1}>Name 1</option>
               <option value={2}>Name 2</option>
@@ -47,10 +45,10 @@ const LoginForm = () => {;
             </select>
           </div>
           <div className="form-group mb-2 row">
-            <label className="form-label col-sm-3 col-md-3 col-lg-3">Password</label>
+            <label className="form-label col-sm-3 col-md-43 col-lg-3">Password</label>
             <input type="password" className="form-control col auto" value={password} onChange={(e) => setPassword(e.target.value)}></input>
           </div>
-          <button type="submit" className="btn btn-success w-100 mt-2" disabled={!validateForm()}>
+          <button type="submit" className="btn btn-success w-100 mt-3"  disabled={!validateForm()}>
             Log In
           </button>
         </form>
